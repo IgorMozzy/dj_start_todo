2cbb9284a0f5e55b105da591410a2014960b3a39
Sqfav <igor.mozzy@outlook.com>
2024-07-20 11:04:53 +0200

    Some refactoring, DRF+drf-yasg added

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,37 @@
-from django.urls import path
-from .views import base
+from django.urls import path, include
+from .views import base_html
+
+from .views import *
+
+from rest_framework.routers import DefaultRouter
+from todolist.views import TaskViewSet, CommentViewSet, TagViewSet
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'tags', TagViewSet)
 
 urlpatterns = [
-    path('', base, name='base'),
+    path('html', base_html, name='base'),
+    path('', include(router.urls)),
+    
+    # # path('tasks/create/', TaskCreateView.as_view(), name='task-create'),
+    # path('tasks/', TaskListView.as_view(), name='task-list'),
+    # path('tasks/<int:pk>/', TaskRetrieveView.as_view(), name='task-retrieve'),
+    # path('tasks/<int:pk>/update/', TaskUpdateView.as_view(), name='task-update'),
+    # path('tasks/<int:pk>/delete/', TaskDestroyView.as_view(), name='task-destroy'),
+
+    # Маршрутизация для работы контроллеров модели Comment
+    # path('comments/create/', CommentCreateView.as_view(), name='comment-create'),
+    # path('comments/', CommentListView.as_view(), name='comment-list'),
+    # path('comments/<int:pk>/', CommentRetrieveView.as_view(), name='comment-retrieve'),
+    # path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    # path('comments/<int:pk>/delete/', CommentDestroyView.as_view(), name='comment-destroy'),
+
+    # Маршрутизация для работы контроллеров модели Tag
+    # path('tags/create/', TagCreateView.as_view(), name='tag-create'),
+    # path('tags/', TagListView.as_view(), name='tag-list'),
+    # path('tags/<int:pk>/', TagRetrieveView.as_view(), name='tag-retrieve'),
+    # path('tags/<int:pk>/update/', TagUpdateView.as_view(), name='tag-update'),
+    # path('tags/<int:pk>/delete/', TagDestroyView.as_view(), name='tag-destroy')
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,9 +1,111 @@
 from django.shortcuts import render
-from .models import Task
+
+from rest_framework import viewsets
+from rest_framework import generics, mixins
+
+from .models import Task, Comment, Tag
+from todolist.serializers import TaskSerializer, CommentSerializer, TagSerializer
+
 
 # Create your views here.
 
 
-def base(request):
+def base_html(request):
     tasks = Task.objects.all()
     return render(request, template_name='todolist/base.html', context={'tasks': tasks})
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+# # Task Views
+# class TaskCreateView(generics.CreateAPIView):
+#     """Класс-контроллер для создания объектов модели Task"""
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+#
+# class TaskListView(generics.ListAPIView):
+#     """Класс-контроллер для просмотра списка объектов модели Task"""
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+#
+#
+# class TaskRetrieveView(generics.RetrieveAPIView):
+#     """Класс-контроллер для просмотра отдельного объекта модели Task"""
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+#
+# class TaskUpdateView(generics.UpdateAPIView):
+#     """Класс-контроллер для редактирования объектов модели Task"""
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+#
+# class TaskDestroyView(generics.DestroyAPIView):
+#     """Класс-контроллер для удаления объектов модели Task"""
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+#
+#
+# # Comment Views
+# class CommentCreateView(generics.CreateAPIView):
+#     """Класс-контроллер для создани объектов модели Comment"""
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+#
+# class CommentListView(generics.ListAPIView):
+#     """Класс-контроллер для просомтра списка объектов модели Comment"""
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+#
+#
+# class CommentRetrieveView(generics.RetrieveAPIView):
+#     """Класс-контроллер для просмотра отдельного объекта модели Comment"""
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+#
+# class CommentUpdateView(generics.UpdateAPIView):
+#     """Класс-контроллер для редактирования объектов модели Comment"""
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+#
+# class CommentDestroyView(generics.DestroyAPIView):
+#     """Класс-контроллер для удаления объектов модели Comment"""
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+#
+#
+# # Tag Views
+# class TagCreateView(generics.CreateAPIView):
+#     """Класс-контроллер для создани объектов модели Tag"""
+#     queryset = Tag.objects.all()
+#     serializer_class = TagSerializer
+#
+# class TagListView(generics.ListAPIView):
+#     """Класс-контроллер для просомтра списка объектов модели Tag"""
+#     queryset = Tag.objects.all()
+#     serializer_class = TagSerializer
+#
+#
+# class TagRetrieveView(generics.RetrieveAPIView):
+#     """Класс-контроллер для просмотра отдельного объекта модели Tag"""
+#     queryset = Tag.objects.all()
+#     serializer_class = TagSerializer
+#
+# class TagUpdateView(generics.UpdateAPIView):
+#     """Класс-контроллер для редактирования объектов модели Tag"""
+#     queryset = Tag.objects.all()
+#     serializer_class = TagSerializer
+#
+# class TagDestroyView(generics.DestroyAPIView):
+#     """Класс-контроллер для удаления объектов модели Comment"""
+#     queryset = Tag.objects.all()
+#     serializer_class = TagSerializer
